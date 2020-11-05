baeb0beaf52817f0438eaa278d9cb5e921f3fdee
Gabriel-Lima232 <68028935+Gabriel-Lima232@users.noreply.github.com>
2020-11-05 18:55:03 +0000

    Add files via upload

--- PortScan.py
+++ PortScan.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
 import socket
-ip = raw_input("Entre com o IP: ")
-port = input("Entre com a PORTA: ")
+
+ip = raw_input("IP TARGET: ")
+port = input("PORT TARGET: ")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if s.connect_ex((ip, port)):
     print "Port", port,"is closed"
